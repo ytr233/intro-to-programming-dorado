72b5e90f22dcf4d6be86bce3413748a910aab7e0
ytr233 <106793842+ytr233@users.noreply.github.com>
2022-10-17 15:31:00 -0700

    javascript partially fixed

--- assets/script.js
+++ assets/script.js
@@ -2,26 +2,37 @@ const abtn = document.getElementById('abtn');
 const ebtn = document.getElementById('ebtn');
 const cbtn = document.getElementById('cbtn');
 
+let show = (event) => {
+    event.target.style.visibility = 'visible';
+   // ebtn.style.visibility = 'visible';
+   // cbtn.style.visibility = 'visible';
+}
 
-abtn.addEventListener('click', () => {
-  // 👇️ hide button
-  abtn.style.visibility = 'hidden';
+let hide = (event) => {
+    event.target.style.visibility = 'hidden';
+    //ebtn.style.visibility = 'hidden';
+    //cbtn.style.visibility = 'hidden';
+}
 
-});
 
-ebtn.addEventListener('click', () => {
+abtn .addEventListener('click', hide);
+ebtn.onclick = function () {
+    hide(event)
+};
+
+//ebtn.addEventListener('click', () => {
     // 👇️ hide button
-    ebtn.style.visibility = 'hidden';
-});
+//    ebtn.style.visibility = 'hidden';
+//});
 
-cbtn.addEventListener('click', () => {
+//cbtn.addEventListener('click', () => {
     // 👇️ hide button
     cbtn.style.visibility = 'hidden';
-});
+//});
 
 // ✅ Show button on hover
 abtn.addEventListener('mouseover', () => {
-    abtn.style.visbility= 'visible';
+  abtn.style.visbility= 'visible';
   
 
-  });
+  //});
